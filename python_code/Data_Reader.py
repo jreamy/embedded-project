84e76ade793e085806f56721327a030209f9402d
Jack Reamy <jreamy17@georgefox.edu>
2020-04-21 12:33:57 -0700

    Uploading functional project code. Uses i2c to serial protocol to simulate i2c devices for development and testing. Allows multiple modes of operation.

--- python_code/Data_Reader.py
+++ python_code/Data_Reader.py
@@ -1,37 +1,65 @@
 
 import serial
 from hand import Hand
+from time import sleep
+from i2c import I2C_commands
+
+def uint16_list(uint8_list):
+    return [(uint8_list[x*2+1]<<8) | uint8_list[x*2]
+        for x in range(int(len(uint8_list)/2))]
+
+def int16_list(uint8_list):
+    print(uint8_list[2:2+2])
+    return [(int.from_bytes(uint8_list[x*2:x*2+2], 'little', signed=True))
+        for x in range(int(len(uint8_list)/2))]
 
 class Data_Reader():
 
     def __init__(self, port='/dev/ttyACM0', baudrate=115200):
         self.ser = serial.Serial(port=port, baudrate=baudrate)
+        self.show = False
 
     def __del__(self):
         self.ser.__del__()
 
     def send(self, text):
         if isinstance(text, (str, Hand)):
-            self.ser.write(text.encode())
+            for char in text:
+                while self.ser.out_waiting:
+                    pass
+                print(char)
+                self.ser.write(char.encode())
 
-    def recieve(self):
+    def receive(self):
         command_char = self.ser.read(1)
         if command_char == b't':
-            data = self.ser.readline(10)
-            return ('t', ''.join([chr(int(x)) for x in data[:-1]]))
+            data = ['d']
+            while data[-1] != '\x00':
+                data.append(chr(self.ser.readline(1)[0]))
+            com = 't'
+            data = ''.join(data[1:-1])
         elif command_char == b'i':
             data = self.ser.read(3)
-            return ('i', [int(x) for x in data])
+            com = 'i'
+            data = [int(x) for x in data]
+        elif command_char == b'e':
+            data = self.ser.read(6)
+            com = 'e'
+            data = uint16_list(data)
         elif command_char == b'h':
-            data = self.ser.read(32)
-            return ('h', Hand([(data[x*2+1]<<8) | data[x*2]
-                for x in range(16)]))
-        elif command_char == b'c':
-            data = self.ser.read(32)
-            return ('c', Hand([(data[x*2+1]<<8) | data[x*2]
-                for x in range(16)]))
+            data = self.ser.read(96)
+            com = 'h'
+            data = Hand(int16_list(data))
+        elif command_char in I2C_commands:
+            self.i2c.write(command_char)
+            return command_char, None
         else:
-            return (command_char, command_char)
+            data = command_char
+            com = command_char
+
+        if self.show:
+            print(com, data)
+        return com, data
 
     def wait(self):
         while not self.available():
@@ -39,3 +67,6 @@ class Data_Reader():
 
     def available(self):
         return self.ser.in_waiting
+
+    def connect_i2c(self, i2c):
+        self.i2c = i2c
--- python_code/hand.py
+++ python_code/hand.py
@@ -1,32 +1,91 @@
 
+class joint():
+    def __init__(self, finger, start_idx):
+        self.data = finger
+        self.idx = start_idx
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.data[self.idx + index]
+        elif index == "x":
+            return self.data[self.idx+0]
+        elif index == "y":
+            return self.data[self.idx+1]
+        elif index == "z":
+            return self.data[self.idx+2]
+        else:
+            raise ValueError("{} not a valid finger index".format(index))
+
+    def __setitem__(self, index, value):
+        print("joint set")
+        if isinstance(index, int):
+            self.data[self.idx + index] = value
+        elif index == "x":
+            self.data[self.idx] = value
+        elif index == "y":
+            self.data[self.idx+1] = value
+        elif index == "z":
+            self.data[self.idx+2] = value
+        else:
+            raise ValueError("{} not a valid joint index".format(index))
+
+    def __setattr__(self, attr, value):
+        if attr in ["x", "y", "z"]:
+            idx = ["x", "y", "z"].index(attr)
+            self[idx] = value
+        else:
+            self.__dict__[attr] = value
+
+class finger():
+    def __init__(self, hand, start_idx):
+        self.data = hand
+        self.idx = start_idx
+        self.base = joint(self.data, self.idx)
+        self.middle = joint(self.data, self.idx+3)
+        self.tip = joint(self.data, self.idx+6)
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.data[self.idx + index]
+        elif index in self.__dict__:
+            return self.__dict__[index]
+        else:
+            raise ValueError("{} not a valid finger index".format(index))
+
+    def __setitem__(self, index, value):
+        print("finger set")
+        if isinstance(index, int):
+            self.data[self.idx + index] = value
+        else:
+            raise ValueError("{} not a valid finger index".format(index))
+
+    def __str__(self):
+        return str(self.data[self.idx:self.idx+9])
+
 class Hand():
 
     def __init__(self, data):
         self.data = data
+        self.thumb = finger(self.data, 9*0)
+        self.pointer = finger(self.data, 9*1)
+        self.middle = finger(self.data, 9*2)
+        self.ring = finger(self.data, 9*3)
+        self.pinky = finger(self.data, 9*4)
+        self.base = joint(self.data, 9*5)
 
     def __getitem__(self, index):
         if isinstance(index, int):
             return self.data[index]
-        elif index == 'thumb':
-            return self.data[0:3]
-        elif index == 'pointer':
-            return self.data[3:6]
-        elif index == "middle":
-            return self.data[6:9]
-        elif index == "ring":
-            return self.data[9:12]
-        elif index == "pinky":
-            return self.data[12:15]
-        elif index == "base":
-            return self.data[15]
+        elif index in self.__dict__:
+            return self.__dict__[index]
         else:
-            raise ValueError("{} not a valid index".format(index))
+            raise ValueError("{} not a valid hand index".format(index))
 
     def __setitem__(self, index, value):
         if isinstance(index, int):
             self.data[index] = value
         else:
-            raise ValueError("{} not a valid index".format(index))
+            raise ValueError("{} not a valid hand index".format(index))
 
     def __str__(self):
         return str(self.data)
